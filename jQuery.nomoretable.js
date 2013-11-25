85966b88d80d49cab48fecf736d59479b281e50c
Kubli <makubli13@gmail.com>
2013-11-25 14:01:08 +0100

    added license and changed variable name

--- jQuery.nomoretable.js
+++ jQuery.nomoretable.js
@@ -1,9 +1,28 @@
+/*Copyright (c) 2013 Martin Kubli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
 (function($)
 {
     $.fn.nomoretable = function(options)
     {
         var defaults = {
-        	"tableclass": "table",
+        	"tablediv": "table",
         	"maxwidth": 700
         };
         var opts = $.extend(defaults, options);
@@ -16,9 +35,9 @@
 	    		if(windowWidth < opts.maxwidth){
 					var text = new Array();
 					var ths = 0;
-					jQuery(opts.tableclass).each(function(table)
+					jQuery(opts.tablediv).each(function(table)
 					{
-						jQuery(opts.tableclass).addClass("nomoretable");
+						jQuery(opts.tablediv).addClass("nomoretable");
 						ths = 0;
 						jQuery(this).find("th").each(function(index){
 							text[index] = jQuery(this).text();
@@ -40,7 +59,7 @@
 						});
 					});
 				}else{
-					jQuery(opts.tableclass).removeClass("nomoretable");
+					jQuery(opts.tablediv).removeClass("nomoretable");
 				}
 			});
         });
